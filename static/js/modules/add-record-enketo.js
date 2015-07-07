861a2554daad7c4c8c7913d4bc1e6e83e51d1a5f
Alex Anderson <anderson@medicmobile.org>
2015-07-07 15:59:43 +0200

    Add actual content for the Enketo form demo

--- static/js/modules/add-record-enketo.js
+++ static/js/modules/add-record-enketo.js
@@ -1,14 +1,30 @@
 (function () {
   'use strict';
   exports.init = function(src) {
-     var btn = $('#send-record-button-enketo');
-     btn.closest('li').removeClass('disabled');
-     btn.on('click', function(e) {
-       e.preventDefault();
-       $('#add-record-panel-enketo .dropdown-menu').toggle();
-     });
-     $('#add-record-panel-enketo .close').on('click', function() {
-       $('#add-record-panel-enketo .dropdown-menu').toggle();
-     });
+    src = 'http://localhost:8080/forms/index.html';
+    if (src) {
+      $.ajax({
+        type: 'head',
+        url: 'http://localhost:8080/forms/index.html',
+        success: function() {
+          var btn = $('#send-record-button-enketo');
+          btn.closest('li').removeClass('disabled');
+          btn.on('click', function(e) {
+            e.preventDefault();
+            $('#add-record-panel-enketo .dropdown-menu').toggle();
+            var iframe = $('#add-record-panel-enketo iframe');
+            if (!iframe.attr('src')) {
+              var url = src + '?xform=' +
+                 encodeURIComponent('https://raw.githubusercontent.com/medic/medic-projects/master/edcd-nepal/forms/Daily%20Hospital%20Surveillance.xml?token=AALsCFAzElEYtrzHWmEabrq4uQY2QAtVks5VpQH_wA%3D%3D');
+              console.log('Pointing iframe at: ' + url);
+              iframe.attr('src', url);
+            }
+          });
+          $('#add-record-panel-enketo .close').on('click', function() {
+            $('#add-record-panel-enketo .dropdown-menu').toggle();
+          });
+        }
+      });
+    }
   };
 }());
